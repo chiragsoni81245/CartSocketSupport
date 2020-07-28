e0599c2345c671cd45ca9c5144b40aa86df6d5a2
Chirag Soni <chiragsoni81245@gmail.com>
2020-07-29 03:53:19 +0530

    set cros to * and improve docker compose file

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app,cors_allowed_origins="http://localhost:3000")
+socketio = SocketIO(app,cors_allowed_origins="*")
 
-from app import routes, models
+from app import routes
 
 if __name__ == '__main__':
     socketio.run(app)
